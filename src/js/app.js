c066f91cdbcc5bded0176983482cc10561f995e0
Mark Henderson <henderson.mark@gmail.com>
2021-07-05 16:01:35 -0400

    fix(app): use 2nd deterministic contract address

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ if (window.ethereum) {
   fetch('./artifact.json')
     .then(res => res.json())
     .then((artifact) => {
-      const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+      const address = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
       const contract = new ethers.Contract(address, artifact.abi, signer)
 
       const filter = contract.filters.Transfer()
