b5fb17610f0cee38b95558180097d14eae2fb56f
Mark Henderson <henderson.mark@gmail.com>
2021-07-06 12:11:37 -0400

    feat(scripts): ingest instaloader output to .build

--- scripts/ingest.js
+++ scripts/ingest.js
@@ -1,23 +1,20 @@
 const fs = require('fs')
-
-const bs58 = require('bs58')
-const { ethers } = require('hardhat')
+const { parse } = require('node-html-parser')
 const { create, globSource } = require('ipfs-http-client')
-
-const bs58toHex = (b58) => `0x${Buffer.from(bs58.decode(b58).slice(2)).toString('hex')}`
 
 const ipfs = create('http://127.0.0.1:5001')
 
 async function main () {
-  const [owner] = await ethers.getSigners()
-
   // TODO: Only jpg for now, other media types later
   const DIR = process.env.INSTA_DEST || './.instaloader'
   const files = fs.readdirSync(DIR).filter(f => f.match(/UTC\.jpg/))
   const timestamps = new Set(files.map(f => f.split('.')[0]))
 
-  const SarkinNFTs = await ethers.getContractFactory('SarkinNFTs')
-  const NFTs = await SarkinNFTs.deploy()
+  const template = fs.readFileSync('./src/index.html')
+  const root = parse(template.toString())
+  const nftsList = root.querySelector('#nfts')
+  nftsList.innerHTML = ''
+  const nftTemplate = root.querySelector('template#nft')
 
   for (let it = timestamps.values(), timestamp = null; timestamp = it.next().value;) { // eslint-disable-line
     // TODO: Better Insta handling parsing
@@ -26,6 +23,13 @@ async function main () {
     try {
       const description = fs.readFileSync(DIR + `/${timestamp}.txt`).toString()
       const hash = await ipfs.add(globSource(DIR + `/${timestamp}.jpg`))
+
+      const item = nftTemplate.innerHTML
+        .replace(/%CID%/, hash.cid.toString())
+        .replace(/%TITLE%/, timestamp)
+        .replace(/%DESC%/, description)
+
+      nftsList.appendChild(parse(item))
       const nftMetadata = {
         title: 'Asset Metadata',
         type: 'object',
@@ -38,14 +42,13 @@ async function main () {
 
       const metadata = await ipfs.add(JSON.stringify(nftMetadata))
       const mintingCID = metadata.cid.toString()
-
-      const tx = await NFTs.mint(owner.address, bs58toHex(mintingCID))
-      await tx.wait()
+      // console.log(mintingCID)
     } catch (err) {
-      console.warn(err.message)
+      // console.warn(err.message)
     }
   }
-  console.log(NFTs.address)
+  console.log(root.toString())
+  fs.writeFileSync('./.build/index.html', root.toString())
 }
 
 main()
