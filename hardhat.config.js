25fec05b6931880da1b6c2b080d328889ba13a9a
Mark Henderson <henderson.mark@gmail.com>
2021-07-11 21:14:58 -0400

    config: turn off hardhat automining

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,10 @@ module.exports = {
   solidity: '0.8.4',
   networks: {
     hardhat: {
+      mining: {
+        auto: false,
+        interval: [500, 2000]
+      },
       forking: {
         url: process.env.ALCHEMY_MAINNET_RPC_URL
       }
