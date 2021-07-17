5a7711dc08859b95f283490322b56e1a5955ad40
Mark Henderson <mark@mrh.io>
2021-07-17 14:19:36 -0400

    feat: create utils.onWindowEthereum

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,15 @@
 export function truncateAddress (address) {
   return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`
 }
+
+export function onWindowEthereum(callback) {
+  if (window.ethereum) {
+    callback();
+  } else {
+    window.addEventListener('ethereum#initialized', callback, {
+      once: true,
+    });
+
+    setTimeout(callback, 3000); // 3 seconds
+  }
+}
