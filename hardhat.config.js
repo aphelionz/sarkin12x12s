66cec6ac65293be3cdcf1d093725ca4751635921
Mark Henderson <henderson.mark@gmail.com>
2021-07-21 21:19:28 -0400

    fix(config): make rinkeby network config env-dependent

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,15 +5,10 @@ require('@nomiclabs/hardhat-waffle')
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+const config = {
   defaultNetwork: 'hardhat',
   solidity: '0.8.4',
   networks: {
-    rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/cjl-KmDLkL3lXgz0s8FtUl9OPFYIK5B2',
-      from: process.env.KEYS_OWNER,
-      accounts: [process.env.KEYS_OWNER, process.env.KEYS_ACCT1]
-    },
     hardhat: {
       mining: {
         auto: false,
